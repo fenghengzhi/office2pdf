56591b4db29a07ff9c5d037c45b577bbf4191f79
fenghengzhi <fenghengzhi@users.noreply.github.com>
2017-02-27 17:46:24 +0800

    Update doc2pdf右键菜单.bat

--- doc2pdf右键菜单.bat
+++ doc2pdf右键菜单.bat
@@ -1,5 +1,5 @@
 @echo off
-reg add HKCR\Word.Document.12\shell\toPDF /ve /f /d "转为PDF"
+reg add HKCR\Word.Document.12\shell\toPDF /ve /f /d "תΪPDF"
 reg add HKCR\Word.Document.12\shell\toPDF\command /ve /d "C:\Windows\SysWOW64\wscript.exe %~dp0doc2pdf.vbs \"%%1\"" /f
-reg add HKCR\Word.Document.8\shell\toPDF /ve /f /d "转为PDF"
+reg add HKCR\Word.Document.8\shell\toPDF /ve /f /d "תΪPDF"
 reg add HKCR\Word.Document.8\shell\toPDF\command /ve /d "C:\Windows\SysWOW64\wscript.exe %~dp0doc2pdf.vbs \"%%1\"" /f
